06a90b9d69a828c031844c79be01b863f89386f6
FGarcia012 <alexander.garcia.sicajau@gmail.com>
2025-10-22 17:40:11 -0600

    add(Creation_date): Se agrego la fecha de creacion de los reportes.

--- models/news.py
+++ models/news.py
@@ -5,7 +5,7 @@ class News(models.Model):
     _name = 'news.report'
     _description = 'Registro de Noticias Internas Distefano'
 
-    start_date = fields.Date(string='Fecha de Inicio', required=True, default=fields.Datetime.now)
+    start_date = fields.Date(string='Fecha de Inicio', required=True, default=fields.Date.today)
     end_date = fields.Date(string='Fecha de Fin') 
     report_description = fields.Text(string='Descripción del reporte', required=True)
     worker = fields.Text(string='Temporal solo nombre del trabajador', required=True)
