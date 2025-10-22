e8653230faf4a9ea46471b650e1ee2aee0321c03
LC2020429 <lcordova-2020429@kinal.edu.gt>
2025-10-22 13:21:49 -0600

    Build: models de noticios y tipo

---
+++ models/news.py
@@ -0,0 +1,11 @@
+from openerp import models, fields, api
+
+class News(models.Model):
+    _name = 'news.report'
+    _description = 'Registro de Noticias Internas Distefano'
+
+    start_date = fields.Date(string='Fecha de Inicio', required=True, default=fields.Datetime.now)
+    end_date = fields.Date(string='Fecha de Fin') 
+    report_description = fields.Text(string='Descripción del reporte', required=True)
+    worker = fields.Text(string='Temporal solo nombre del trabajador', required=True)
+    type_id = fields.Many2one('news.type', string='Tipo de noticia', required=True)
---
+++ models/type.py
@@ -0,0 +1,7 @@
+from openerp import models, fields, api
+
+class NewsType(models.Model):
+    _name = 'news.type'
+    _description = 'Tipo de Noticias Internas Distefano'
+
+    name = fields.Char(string='Nombre', required=True)
