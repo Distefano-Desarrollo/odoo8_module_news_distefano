0081cf561e464c391db0fda91d78ed0d8bc5eea9
LC2020429 <lcordova-2020429@kinal.edu.gt>
2025-10-22 16:59:46 -0600

    propuesta

--- __openerp__.py
+++ __openerp__.py
@@ -7,7 +7,13 @@
     'summary': 'Modulo para gestionar noticias internas de la empresa',
     'depends': ['base', 'hr','web'],
     'data':[
+        'views/actions.xml',
         'views/menu.xml',
+        'views/type/tree.xml',
+        'views/type/form.xml',
+        'views/new/tree.xml',
+        'views/new/form.xml',
+        'views/new/calendar.xml',
     ],
     'installable': True,
     'auto_install': False,
--- models/news.py
+++ models/news.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from openerp import models, fields, api
 
 class News(models.Model):
@@ -6,6 +7,6 @@ class News(models.Model):
 
     start_date = fields.Date(string='Fecha de Inicio', required=True, default=fields.Datetime.now)
     end_date = fields.Date(string='Fecha de Fin') 
-    report_description = fields.Text(string='Descripción del reporte', required=True)
-    worker = fields.Text(string='Temporal solo nombre del trabajador', required=True)
+    report_description = fields.Text(string='Descripción del reporte', required=True, default='Nueva noticia interna')
+    worker = fields.Text(string='Temporal solo nombre del trabajador', required=True, default='Nombre del trabajador')
     type_id = fields.Many2one('news.type', string='Tipo de noticia', required=True)
