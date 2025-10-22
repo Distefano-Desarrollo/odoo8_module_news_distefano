1bdd13ff94010111400ed405f809985e0c50ffee
JH1018 <javierherrera5513@gmail.com>
2025-10-22 16:50:03 -0600

    style(noticias): diseño

--- __openerp__.py
+++ __openerp__.py
@@ -7,6 +7,11 @@
     'summary': 'Modulo para gestionar noticias internas de la empresa',
     'depends': ['base', 'hr','web'],
     'data':[
+        'views/type/typeForm.xml',
+        'views/type/treeType.xml',
+        'views/news/newsForm.xml',
+        'views/news/treeNews.xml',
+        'views/news/calendarNews.xml',
         'views/menu.xml',
     ],
     'installable': True,
