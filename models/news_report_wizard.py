63821dcdd17afb1319d3837ab8bdfb09341ed466
LC2020429 <lcordova-2020429@kinal.edu.gt>
2025-10-23 17:46:47 -0600

    Fix: formato de la fecha a español

--- models/news_report_wizard.py
+++ models/news_report_wizard.py
@@ -12,6 +12,7 @@ from datetime import datetime
 from reportlab.platypus import Image
 from reportlab.lib.units import inch
 from openerp.modules import get_module_resource
+from babel.dates import format_date  
 
 class NewsReportWizard(models.TransientModel):
     _name = 'odoo8_module_news_distefano.news_report_wizard'
@@ -24,7 +25,6 @@ class NewsReportWizard(models.TransientModel):
         help='Selecciona el registro de noticia desde el cual generar el reporte'
     )
 
-    
     file_data = fields.Binary('PDF data', readonly=True)
     file_name = fields.Char('Archivo', size=64)
     
@@ -36,8 +36,6 @@ class NewsReportWizard(models.TransientModel):
         if active_id:
             res['new_id'] = active_id
         return res
-
-    
     @api.multi
     def generate_news_pdf(self):
         """Genera el PDF de noticias agrupadas por mes para un empleado"""
@@ -84,7 +82,7 @@ class NewsReportWizard(models.TransientModel):
                 "<b>Código:</b> {0}".format(news_base.name),
                 "<b>Cargo:</b> {0}".format(employee.job_id.name if employee.job_id else "N/A"),
                 "<b>Departamento:</b> {0}".format(employee.department_id.name if employee.department_id else "N/A"),
-                "<b>Generado el:</b> {0}".format(datetime.now().strftime('%d/%m/%Y'))
+                "<b>Generado el:</b> {0}".format(datetime.now().strftime('%d/%m/%y'))
             ]
 
             for info in employee_info:
@@ -94,7 +92,7 @@ class NewsReportWizard(models.TransientModel):
             news_by_month = {}
             for news in news_records:
                 if news.start_date:
-                    month_year = fields.Date.from_string(news.start_date).strftime("%B %Y")
+                    month_year = format_date(fields.Date.from_string(news.start_date), format='MMMM yyyy', locale='es_ES')  # Formato en español
                     if month_year not in news_by_month:
                         news_by_month[month_year] = []
                     news_by_month[month_year].append(news)
@@ -109,8 +107,8 @@ class NewsReportWizard(models.TransientModel):
                 data = [['Fecha Inicio', 'Fecha Fin', 'Tipo', 'Descripción']]
                 for news in news_by_month[month]:
                     data.append([
-                        news.start_date or "",
-                        news.end_date or "",
+                        datetime.strptime(news.start_date, '%Y-%m-%d').strftime('%d/%m/%Y') if news.start_date else "",
+                        datetime.strptime(news.end_date, '%Y-%m-%d').strftime('%d/%m/%Y') if news.end_date else "",
                         news.type_id.name or "",
                         news.description or ""
                     ])
